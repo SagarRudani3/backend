1b7bad4539ba3040ce9854d96146a17528038d71
SagarRudani3 <sagar.rudani88@gmail.com>
2025-04-08 12:08:59 +0530

    deployment check

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ const emailRoutes = require('./routes/email');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Initialize Agenda
 const agenda = new Agenda({ db: { address: process.env.MONGODB_URI } });
 
-// Configure nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
@@ -28,18 +25,14 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// Make agenda and transporter available throughout the app
 app.locals.agenda = agenda;
 app.locals.transporter = transporter;
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/emails', emailRoutes);
 
-// Define Agenda job
 agenda.define('send email', async (job) => {
   const { to, subject, body } = job.attrs.data;
 
@@ -51,23 +44,21 @@ agenda.define('send email', async (job) => {
      html: body
     });
     console.log("%c Line:48 🍔 sendMail", "color:#fca650", sendMail);
-    
+
     console.log(`Email sent to ${to}`);
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.log("%c Line:50 🍔 error", "color:#ffdd4d", error);
     throw error;
   }
 });
 
-// Start Agenda
 (async function() {
   await agenda.start();
   console.log('Agenda started');
 })();
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-module.exports = app; // For testing purposes
+module.exports = app; 
